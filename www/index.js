790f0ece94b6e1ecbaed1216a05b63d21edc7927
Andrei Kuchuk <kreezag@gmail.com>
2024-09-03 00:37:49 +0400

    add base logic of game of life

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
-import * as wasm from "game-of-life";
+import { Universe } from "game-of-life";
 
-wasm.greet("Hello world!");
+console.log('>> Universe', Universe)
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
